5538795a4f116ec933811f15614dd796f65b7f1c
satyamtiwari1004 <satyamtiwari1004@gmail.com>
2024-01-01 01:14:57 +0530

    Completed Skills Section & logic to fetch data from yaml file

--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .data import experience_data,skills_data,other_projects_data
 
-# Create your views here.
 def load_home(request):
-    return render(request,"index.html")
+    exp_data=experience_data()
+    ski_dat=skills_data()
+    oth_pro_data=other_projects_data()
+    context={"exp_data":exp_data,"ski_dat":ski_dat,"oth_pro_data":oth_pro_data}
+    return render(request,"index.html",context)
 
+def load_data(request):
+    exp_data=experience_data()
+    ski_dat=skills_data()
+    oth_pro_data=other_projects_data()
+    print(exp_data)
+    context={"exp":exp_data,"ski_dat":ski_dat,"oth_pro_data":oth_pro_data}
+    print(context)
+    return JsonResponse({"context":context},status=200)
