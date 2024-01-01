8577a6c5d87f0796f1b78dc261aa4fe65ff68c70
satyamtiwari1004 <satyamtiwari1004@gmail.com>
2024-01-01 23:55:10 +0530

    Added Projects & Experience Sections

---
+++ portfolio/static/js/loading.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector(
+            "#spinners").style.visibility = "hidden";
+          document.querySelector(
+            "body").style.visibility = "visible";
+    }, 2000); // Adjust the time as needed
+});
