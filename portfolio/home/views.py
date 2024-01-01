41e3639818e6dfb5a4524bc13b6ec0d8b115245e
satyamtiwari1004 <satyamtiwari1004@gmail.com>
2024-01-01 23:55:10 +0530

    Added Projects & Experience Sections

--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .data import experience_data,skills_data,other_projects_data
+from .data import experience_data,skills_data,other_projects_data,social_data
 
 def load_home(request):
     exp_data=experience_data()
     ski_dat=skills_data()
     oth_pro_data=other_projects_data()
-    context={"exp_data":exp_data,"ski_dat":ski_dat,"oth_pro_data":oth_pro_data}
+    soc_data=social_data()
+    context={"exp_data":exp_data[0],"ski_dat":ski_dat[0],"oth_pro_data":oth_pro_data,"soc_data":soc_data}
     return render(request,"index.html",context)
 
 def load_data(request):
     exp_data=experience_data()
     ski_dat=skills_data()
     oth_pro_data=other_projects_data()
-    print(exp_data)
-    context={"exp":exp_data,"ski_dat":ski_dat,"oth_pro_data":oth_pro_data}
-    print(context)
+    soc_data=social_data()
+    context={"exp_data":exp_data[0],"ski_dat":ski_dat[0],"oth_pro_data":oth_pro_data,"soc_data":soc_data}
     return JsonResponse({"context":context},status=200)
